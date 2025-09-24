8085630d325165b6ed7e2d1f6bd89c945cf70145
Tiago <tiagorockman@gmail.com>
2025-09-24 13:26:19 -0300

    week 4

--- controllers/accountController.js
+++ controllers/accountController.js
@@ -1,5 +1,7 @@
 const utilities = require("../utilities/")
 const accountModel = require("../models/account-model")
+//password hash
+const bcrypt = require("bcryptjs")
 
 /* ****************************************
 *  Deliver login view
@@ -32,11 +34,25 @@ async function registerAccount(req, res) {
   let nav = await utilities.getNav()
   const { account_firstname, account_lastname, account_email, account_password } = req.body
 
+  // Hash the password before storing
+  let hashedPassword
+  try {
+    // regular password and cost (salt is generated automatically)
+    hashedPassword = await bcrypt.hashSync(account_password, 10)
+  } catch (error) {
+    req.flash("notice", 'Sorry, there was an error processing the registration.')
+    res.status(500).render("account/register", {
+      title: "Registration",
+      nav,
+      errors: null,
+    })
+  }
+
   const regResult = await accountModel.registerAccount(
     account_firstname,
     account_lastname,
     account_email,
-    account_password
+    hashedPassword
   )
 
   if (regResult) {
@@ -50,7 +66,7 @@ async function registerAccount(req, res) {
     })
   } else {
     req.flash("notice", "Sorry, the registration failed.")
-    res.status(501).render("account/register", {
+    res.status(501).render("account/registration", {
       title: "Registration",
       nav,
     })
--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -2,6 +2,7 @@ const express = require("express")
 const router = new express.Router() 
 const accountController = require("../controllers/accountController")
 const utilities = require("../utilities/")
+const regValidate = require('../utilities/account-validation')
 
 // Route to build account management view
 router.get("/login", utilities.handleErrors(accountController.buildLogin));
@@ -9,7 +10,10 @@ router.get("/login", utilities.handleErrors(accountController.buildLogin));
 // Route to build account management view
 router.get("/registration", utilities.handleErrors(accountController.buildregistration));
 
-router.post('/register', utilities.handleErrors(accountController.registerAccount))
+router.post('/register', regValidate.registationRules(), regValidate.checkRegData,
+             utilities.handleErrors(accountController.registerAccount))
 
+// Process the login attempt
+router.post("/login",  (req, res) => { res.status(200).send('login process') })
 
 module.exports = router;
